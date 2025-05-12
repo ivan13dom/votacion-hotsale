bcc7baca3cf22b95361b66528e90f3baa3afbcd1
idominguez <ivan_dominguez2016@hotmail.com>
2025-05-12 11:54:03 -0300

    limitador de votos + dashboard

--- app.py
+++ app.py
@@ -62,8 +62,9 @@ def gracias():
     return render_template('gracias.html')
 
 if __name__ == '__main__':
-    crear_tabla()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 @app.route('/dashboard')
 def dashboard():
